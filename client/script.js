671e2012b1df7676abb94cac73c54c69da56a965
Emmanuel Izilov <em5750@gmail.com>
2021-09-30 18:34:28 -0400

    Intermediate solutions added

--- client/script.js
+++ client/script.js
@@ -88,7 +88,12 @@ const ohMy = () => {
     // YOUR CODE HERE
     axios.get('http://localhost:3000/animals')
         .then( res => {
-            console.log(res.data)
+            // console.log(res.data)
+            for (let i = 0; i < res.data.length; i++){
+                const newParagraph = document.createElement('p')
+                newParagraph.textContent = res.data[i]
+                document.body.appendChild(newParagraph)
+            }
         })
 }
 
@@ -153,7 +158,7 @@ repeatBtn.addEventListener('click', repeatMyParam)
 
 // CODE HERE
 function queryTest(){
-    axios.get('http://localhost:3000/query-test/?query=Happy-Coder')
+    axios.get('http://localhost:3000/query-test/?query=Happy-Coder&anotherQuery=second-query')
         .then(res => {
             console.log(res.data)
         })
@@ -198,21 +203,29 @@ queryBtn.addEventListener('click', queryTest)
 
 //PROBLEM 11
 /*
-    You are going to add the ability to POST to the server. You'll need to create a small form and write a function that makes a post request. Then you'll attach that function to the submit event on the form. We'll be creating a list of foods. 
+    You are going to add the ability to POST to the server. You'll need to create a small form and write 
+    a function that makes a post request. Then you'll attach that function to the submit event on the form. 
+    We'll be creating a list of foods. 
 
-    In the index.html file inside of the client folder, create a form with one text input field and a button. The input field should have a placeholder that tells the user to enter a food. And the button should indicate that it will add food into a list. 
+    In the index.html file inside of the client folder, create a form with one text input field and a button. 
+    The input field should have a placeholder that tells the user to enter a food. And the button should indicate 
+    that it will add food into a list. 
 
     In this file (script.js), create a function called createFood. 
     
     Inside the function, select the input you just created in the HTML and save it to a variable called foodInput. 
     
-    Next, create an object called body inside the function. It should have one key-value pair. The key should be newFood (make sure to match the case and spelling exactly) and the value should be the value of the food input. 
+    Next, create an object called body inside the function. It should have one key-value pair. The key should be
+     newFood (make sure to match the case and spelling exactly) and the value should be the value of the food input. 
 
-    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in body as the second argument. 
+    Now make an axios post request to /food. Inside the parentheses where you passed the URL in, pass in body as 
+    the second argument. 
 
-    Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. Inside that callback, console log the res.data. 
+    Use a .then to handle the promise returned from the axios call. Pass a callback function to the .then. 
+    Inside that callback, console log the res.data. 
 
-    Based on what we did earlier to display this type of data, write code that will display the response in your HTML document. 
+    Based on what we did earlier to display this type of data, write code that will display the response in 
+    your HTML document. 
 */
 
 // CODE HERE 
